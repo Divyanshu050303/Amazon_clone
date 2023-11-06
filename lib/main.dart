19e6f59b79c86b61dff6beecfedb52c89c314558
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-06 22:31:50 +0530

    Dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,45 @@
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:youtube_clone/Providers/user_provider.dart';
 import 'package:youtube_clone/constants/global_variable.dart';
 import 'package:youtube_clone/features/auth/screen/auth_screen.dart';
 import 'package:youtube_clone/router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+      ChangeNotifierProvider(create: (context)=>UserProvider())
+  ], child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-         primaryColor: GlobalVariables.backgroundColor,
-        colorScheme: const ColorScheme.light(
-          primary: GlobalVariables.secondaryColor
-        ),
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          iconTheme: IconThemeData(
-            color: Colors.black
+          primaryColor: GlobalVariables.backgroundColor,
+          colorScheme: const ColorScheme.light(
+              primary: GlobalVariables.secondaryColor
+          ),
+          appBarTheme: const AppBarTheme(
+              elevation: 0,
+              iconTheme: IconThemeData(
+                  color: Colors.black
+              )
           )
-        )
-        ),
+      ),
 
-      onGenerateRoute: (settings)=>generateRoute(settings),
-      home:  const AuthScreen(),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
