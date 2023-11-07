f7c192f6564f8ff7279b6a356427b50e1be24330
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-07 23:20:55 +0530

    order

---
+++ lib/features/Account/Widget/orders.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_clone/constants/global_variable.dart';
+import 'package:youtube_clone/features/Account/Widget/SingleProduct.dart';
+
+class Orders extends StatefulWidget {
+  const Orders({super.key});
+
+  @override
+  State<Orders> createState() => _OrdersState();
+}
+
+class _OrdersState extends State<Orders> {
+  List list = [
+    "https://images.unsplash.com/photo-1699262289693-ff0c49b87fba?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8",
+    "https://images.unsplash.com/photo-1699262289693-ff0c49b87fba?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8",
+    "https://images.unsplash.com/photo-1699262289693-ff0c49b87fba?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8",
+    "https://images.unsplash.com/photo-1699262289693-ff0c49b87fba?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 15),
+              child: const Text(
+                "Your Order",
+                style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 15),
+              child: Text(
+                "See all",
+                style: TextStyle(
+                    fontSize: 18,
+                    color: GlobalVariables.selectedNavBarColor,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+          ],
+        ),
+        // to display a product we create a list view builder
+        Container(
+          height: 170,
+          padding: const EdgeInsets.only(left: 10, top: 20, right: 0),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                return SingleProduct(image: list[index]);
+              }),
+        )
+      ],
+    );
+  }
+}
