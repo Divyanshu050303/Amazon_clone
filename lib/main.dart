25f37a6f6d6684f92ab7dd0794988db14eeadf99
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-07 23:22:19 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:youtube_clone/Providers/user_provider.dart';
+import 'package:youtube_clone/comman/widget/bottom_bar.dart';
 import 'package:youtube_clone/constants/global_variable.dart';
+import 'package:youtube_clone/features/Home/Screen/HomeScreen.dart';
 import 'package:youtube_clone/features/auth/screen/auth_screen.dart';
+import 'package:youtube_clone/features/auth/screen/services/auth_services.dart';
 import 'package:youtube_clone/router.dart';
 
 void main() {
-  runApp(MultiProvider(providers: [
-      ChangeNotifierProvider(create: (context)=>UserProvider())
-  ], child: MyApp()));
+  runApp(MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => UserProvider())],
+      child: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -21,27 +23,30 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  final AuthService authService = AuthService();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    authService.getUserData(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
           primaryColor: GlobalVariables.backgroundColor,
-          colorScheme: const ColorScheme.light(
-              primary: GlobalVariables.secondaryColor
-          ),
+          colorScheme:
+              const ColorScheme.light(primary: GlobalVariables.secondaryColor),
           appBarTheme: const AppBarTheme(
-              elevation: 0,
-              iconTheme: IconThemeData(
-                  color: Colors.black
-              )
-          )
-      ),
-
+              elevation: 0, iconTheme: IconThemeData(color: Colors.black))),
       onGenerateRoute: (settings) => generateRoute(settings),
-      home: const AuthScreen(),
+      home: Provider.of<UserProvider>(context).user.token.isNotEmpty
+          ? const BottomBar()
+          : const AuthScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
