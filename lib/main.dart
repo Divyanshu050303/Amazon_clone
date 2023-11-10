7fe9753ae1ead2608aacbebd205d9323c156a802
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-10 23:08:40 +0530

    add the condition to check the user is logged in or not if not navigate to the sign in screen else navigate to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,35 @@
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:youtube_clone/Providers/user_provider.dart';
-import 'package:youtube_clone/comman/widget/bottom_bar.dart';
-import 'package:youtube_clone/constants/global_variable.dart';
-import 'package:youtube_clone/features/Home/Screen/HomeScreen.dart';
-import 'package:youtube_clone/features/auth/screen/auth_screen.dart';
-import 'package:youtube_clone/features/auth/screen/services/auth_services.dart';
 import 'package:youtube_clone/router.dart';
 
+import 'Providers/user_provider.dart';
+import 'comman/widget/bottom_bar.dart';
+import 'constants/global_variable.dart';
+import 'features/admin/Screen/adminScreen.dart';
+import 'features/auth/screen/auth_screen.dart';
+import 'features/auth/screen/services/auth_services.dart';
+
 void main() {
-  runApp(MultiProvider(
-      providers: [ChangeNotifierProvider(create: (context) => UserProvider())],
-      child: MyApp()));
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+    ),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
   final AuthService authService = AuthService();
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     authService.getUserData(context);
   }
@@ -35,18 +37,27 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Amazon Clone',
       theme: ThemeData(
-          primaryColor: GlobalVariables.backgroundColor,
-          colorScheme:
-              const ColorScheme.light(primary: GlobalVariables.secondaryColor),
-          appBarTheme: const AppBarTheme(
-              elevation: 0, iconTheme: IconThemeData(color: Colors.black))),
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+        useMaterial3: true, // can remove this line
+      ),
       onGenerateRoute: (settings) => generateRoute(settings),
       home: Provider.of<UserProvider>(context).user.token.isNotEmpty
-          ? const BottomBar()
+          ? Provider.of<UserProvider>(context).user.type == 'admin'
+          ? const AdminScreen()
+          : const BottomBar()
           : const AuthScreen(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
