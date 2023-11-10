f5f1fe9437da5702ee18b671f628e036cb9de72f
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-10 17:39:42 +0530

    align file

--- lib/features/Account/Widget/belowAppbar.dart
+++ lib/features/Account/Widget/belowAppbar.dart
@@ -1,42 +1,45 @@
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:youtube_clone/Providers/user_provider.dart';
-import 'package:youtube_clone/constants/global_variable.dart';
+
+import '../../../Providers/user_provider.dart';
+import '../../../constants/global_variable.dart';
 
 class BelowAppBar extends StatelessWidget {
-  const BelowAppBar({super.key});
+  const BelowAppBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final user=Provider.of<UserProvider>(context).user;
+    final user = Provider.of<UserProvider>(context).user;
+
     return Container(
       decoration: const BoxDecoration(
-        gradient: GlobalVariables.appBarGradient
+        gradient: GlobalVariables.appBarGradient,
       ),
       padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-      child:Row(
+      child: Row(
         children: [
           RichText(
-            text:   TextSpan(
-              text: "Hello, ",
-                style: const TextStyle(
+            text: TextSpan(
+              text: 'Hello, ',
+              style: const TextStyle(
                 fontSize: 22,
-                  color: Colors.black
-            ),
+                color: Colors.black,
+              ),
               children: [
                 TextSpan(
-                text:  user.name,
-                style: const TextStyle(
+                  text: user.name,
+                  style: const TextStyle(
                     fontSize: 22,
                     color: Colors.black,
-                  fontWeight: FontWeight.w600
-                ),)
-              ]
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
             ),
-
           ),
         ],
-      ) ,
+      ),
     );
   }
 }
