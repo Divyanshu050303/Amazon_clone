9f34c2c9ff821315d370964571c200e21d8f7e08
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-10 17:49:07 +0530

    add the various widget

--- lib/features/Home/Screen/HomeScreen.dart
+++ lib/features/Home/Screen/HomeScreen.dart
@@ -1,21 +1,118 @@
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:youtube_clone/Providers/user_provider.dart';
+import 'package:youtube_clone/features/Home/widget/allProduct.dart';
+
+import '../../../constants/global_variable.dart';
+import '../../search/Screen/SearchScreen.dart';
+import '../widget/Address_box.dart';
+import '../widget/carousel_image.dart';
+import '../widget/deal_of_the_day.dart';
+import '../widget/top_categories.dart';
 
 class HomeScreen extends StatefulWidget {
-  static const String routeName='/name';
-  const HomeScreen({super.key});
+  static const String routeName = '/home';
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  void navigateToSearchScreen(String query) {
+    Navigator.pushNamed(context, SearchScreen.routeName, arguments: query);
+  }
+
   @override
   Widget build(BuildContext context) {
-  final user=Provider.of<UserProvider>(context).user;
-    return Center(
-      child: Text(user.toJson()),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: GlobalVariables.appBarGradient,
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Container(
+                  height: 42,
+                  margin: const EdgeInsets.only(left: 15),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(7),
+                    elevation: 1,
+                    child: TextFormField(
+                      onFieldSubmitted: navigateToSearchScreen,
+                      decoration: InputDecoration(
+                        prefixIcon: InkWell(
+                          onTap: () {},
+                          child: const Padding(
+                            padding: EdgeInsets.only(
+                              left: 6,
+                            ),
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.black,
+                              size: 23,
+                            ),
+                          ),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: const EdgeInsets.only(top: 10),
+                        border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(7),
+                          ),
+                          borderSide: BorderSide.none,
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(7),
+                          ),
+                          borderSide: BorderSide(
+                            color: Colors.black38,
+                            width: 1,
+                          ),
+                        ),
+                        hintText: 'Search Amazon.in',
+                        hintStyle: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.transparent,
+                height: 42,
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                child: const Icon(Icons.mic, color: Colors.black, size: 25),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            AddressBox(),
+            SizedBox(height: 10),
+            TopCategories(),
+            SizedBox(height: 10),
+            CarouselImage(),
+            SizedBox(height: 10),
+            DealOfDay(),
+            SizedBox(height: 10),
+            // AllProduct()
+          //
+          ],
+        ),
+      ),
     );
   }
 }
