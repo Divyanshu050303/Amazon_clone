253dc43d99a9d48944d53629f5221456c9eb614d
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-10 17:31:14 +0530

    format the file

--- lib/features/Account/Widget/account_button.dart
+++ lib/features/Account/Widget/account_button.dart
@@ -3,29 +3,37 @@ import 'package:flutter/material.dart';
 class AccountButton extends StatelessWidget {
   final String text;
   final VoidCallback onTap;
-
-  const AccountButton({super.key, required this.text, required this.onTap});
+  const AccountButton({
+    Key? key,
+    required this.text,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10),
+        margin: const EdgeInsets.symmetric(horizontal: 10),
         height: 40,
         decoration: BoxDecoration(
-            border: Border.all(color: Colors.white, width: 0),
-            borderRadius: BorderRadius.circular(50),
-            color: Colors.white),
+          border: Border.all(color: Colors.white, width: 0.0),
+          borderRadius: BorderRadius.circular(50),
+          color: Colors.white,
+        ),
         child: OutlinedButton(
           style: ElevatedButton.styleFrom(
-              primary: Colors.black.withOpacity(0.03),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50),
-              )),
+            primary: Colors.black12.withOpacity(0.03),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50),
+            ),
+          ),
           onPressed: onTap,
           child: Text(
             text,
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.normal),
+            style: const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.normal,
+            ),
           ),
         ),
       ),
