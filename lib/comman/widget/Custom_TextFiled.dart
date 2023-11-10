b3693a3a1a9d5798c2163962bd0e1a1e9f2a33fe
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-10 17:47:24 +0530

    update the border radius

--- lib/comman/widget/Custom_TextFiled.dart
+++ lib/comman/widget/Custom_TextFiled.dart
@@ -3,19 +3,19 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
-
-  // final Icons icon;
+  final int maxLines;
   const CustomTextField({
-    super.key,
+    Key? key,
     required this.controller,
     required this.hintText,
-  });
+    this.maxLines = 1,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
     return Padding(
-      padding: EdgeInsets.only(
+      padding:    EdgeInsets.only(
           left: mediaQueryData.size.width * 0.05,
           right: mediaQueryData.size.width * 0.05),
       child: TextFormField(
@@ -23,17 +23,22 @@ class CustomTextField extends StatelessWidget {
         decoration: InputDecoration(
             hintText: hintText,
             border:   OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-                borderSide: const BorderSide(color: Colors.black)),
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide(
+                  color: Colors.black38,
+                )),
             enabledBorder:   OutlineInputBorder(
                 borderRadius: BorderRadius.circular(15),
-                borderSide: BorderSide(color: Colors.black))),
+                borderSide: BorderSide(
+                  color: Colors.black38,
+                ))),
         validator: (val) {
-          if(val==null || val.isEmpty){
-            return 'Enter you $hintText';
+          if (val == null || val.isEmpty) {
+            return 'Enter your $hintText';
           }
           return null;
         },
+        maxLines: maxLines,
       ),
     );
   }
